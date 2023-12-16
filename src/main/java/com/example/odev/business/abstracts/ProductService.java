b4061d38b6b34bebc0340009e032cf703dd03865
Yiğit Özyılmaz <keremyigit1995@gmail.com>
2023-12-16 15:28:14 +0300

    image kolonu ekledim push çalışıyor

--- src/main/java/com/example/odev/business/abstracts/ProductService.java
+++ src/main/java/com/example/odev/business/abstracts/ProductService.java
@@ -2,11 +2,15 @@ package com.example.odev.business.abstracts;
 
 import com.example.odev.business.responses.GetAllProducts;
 import com.example.odev.business.responses.GetProductsDetails;
-
+import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
+import com.example.odev.Entity.Category;
 
 public interface ProductService {
+
+
     List<GetAllProducts> getAll();
 
     List<GetProductsDetails> findAll();
+    void saveProductToDB(MultipartFile address, String name, String explanation, Double price, Long category_id) ;
 }
--- src/main/java/com/example/odev/webApiControllers/ProductsControllers.java
+++ src/main/java/com/example/odev/webApiControllers/ProductsControllers.java
@@ -3,12 +3,12 @@ package com.example.odev.webApiControllers;
 import com.example.odev.business.abstracts.ProductService;
 import com.example.odev.business.responses.GetAllProducts;
 import com.example.odev.business.responses.GetProductsDetails;
+import com.example.odev.business.responses.ProductRequest;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -31,5 +31,12 @@ public class ProductsControllers {
         return productService.findAll();
     }
 
+
+
+    @PostMapping("/addP")
+    public String saveProduct(@RequestBody ProductRequest request) {
+        productService.saveProductToDB(request.getAddress(), request.getName(), request.getExplanation(), request.getPrice(), request.getCategory_id());
+        return "redirect:/listProduct.html";
+    }
 
 }
